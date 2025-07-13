49a0650c2be642ca7f088af34a530cd43ca36cd3
provokateurin <kate@provokateurin.de>
2025-07-13 18:44:37 +0200

    build: Update everything to be able to build and install it today

---
+++ app/build.gradle.kts
@@ -0,0 +1,42 @@
+import org.gradle.kotlin.dsl.support.kotlinCompilerOptions
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.example.muzei.muzeiapod"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.muzei.muzeiapod"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(libs.muzei.api)
+    implementation(libs.jsoup)
+}
